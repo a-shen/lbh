1c32407fd8095d54befd4d89c2c858b8dc65c3be
Amy Shen <amyshen@gmail.com>
2013-06-12 13:19:21 -0700

    Added iframe for commenting system

--- LBH/Views.hs
+++ LBH/Views.hs
@@ -456,6 +456,8 @@ showPost muser post = do
       P.writeHtml wopts $
        let md = P.readMarkdown ropts (T.unpack . crlf2lf $ postBody post)
        in extractActieCodeBlocks md
+  let path = (lbhUrl ++ "/" ++ (show $ postId post) ++ "/comments")
+  iframe ! src (toValue path) ! A.style "overflow: scroll;" ! width "1000" ! height "500" $ ""
      where ropts = P.def { P.readerExtensions     = P.githubMarkdownExtensions }
            wopts = P.def { P.writerHighlight      = True
                          , P.writerHighlightStyle = P.kate
